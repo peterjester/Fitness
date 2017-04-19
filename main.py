c1e6305c12a336a3c77e564e185cabe7276b8853
Peter <peter.j.jester@gmail.com>
2017-04-18 20:48:51 -0400

    ideas and todo comments added

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ some_url = 'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json'
 
 sample_url = 'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec/time/00:00/00:01.json'
 
-
-
 url = test_url
 filename = 'HR' + timeStr + '.json'
 
@@ -33,6 +31,10 @@ print 'requesting ' + url
 response = requests.get(url=url, headers={'Authorization':'Bearer ' + token})
 
 jsonString = response.text
+
+#todo - take this jsonString and parse out the most recent heart rate value
+#todo - implement a subscriber to know when the heart rate is updated.
+#idea - progress bar for calories burned in a day vs calories eaten in a day or some variation
 
 # eval ensures our json string is not double encoded
 print json.dumps(eval(jsonString), indent=4)
